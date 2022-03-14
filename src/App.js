9f74a615a903b97f9f76997ebd2e0491b1312068
이승원 <iseung-won@iseung-won-ui-MacBookPro.local>
2022-03-15 05:24:10 +0900

    22/03/15 mk renderItem working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Agenda } from 'react-native-calendars';
+import { TouchableOpacity } from 'react-native';
+import { Card, Paragraph, Avatar } from 'react-native-paper';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -8,98 +10,70 @@ const Container = styled.SafeAreaView`
 
 const View = styled.View``;
 
-const Text = styled.Text``;
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 
 const App = () => {
-  const [items, setItems] = useState(true);
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+
+        if (!items[strTime]) {
+          items[strTime] = [];
+
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              day: strTime,
+            });
+          }
+        }
+      }
+
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Paragraph>{item.name}</Paragraph>
+              <Avatar.Text label="G" />
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <Container>
       <Agenda
-        // The list of items that have to be displayed in agenda. If you want to render item as empty date
-        // the value of date key has to be an empty array []. If there exists no value for date key it is
-        // considered that the date in question is not yet loaded
-        items={{
-          '2022-03-22': [{ name: 'item 1 - any js object' }],
-          '2022-03-25': [
-            { name: 'item 3 - any js object' },
-            { name: 'any js object' },
-          ],
-        }}
-        // Callback that gets called when items for a certain month should be loaded (month became visible)
-        loadItemsForMonth={(month) => {
-          console.log('trigger items loading');
-        }}
-        // Callback that fires when the calendar is opened or closed
-        onCalendarToggled={(calendarOpened) => {
-          console.log(calendarOpened);
-        }}
-        // Callback that gets called on day press
-        onDayPress={(day) => {
-          console.log('day pressed');
-        }}
-        // Callback that gets called when day changes while scrolling agenda list
-        onDayChange={(day) => {
-          console.log('day changed');
-        }}
-        // Initially selected day
-        selected={'2022-03-10'}
-        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-        minDate={'2012-05-10'}
-        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-        maxDate={'2022-05-30'}
-        // Max amount of months allowed to scroll to the past. Default = 50
-        pastScrollRange={50}
-        // Max amount of months allowed to scroll to the future. Default = 50
-        futureScrollRange={50}
-        // Specify how each item should be rendered in agenda
-        renderItem={(item, firstItemInDay) => {
-          return <View />;
-        }}
-        // Specify how each date should be rendered. day can be undefined if the item is not first in that day
-        renderDay={(day, item) => {
-          return <View />;
-        }}
-        // Specify how empty date content with no items should be rendered
-        renderEmptyDate={() => {
-          return <View />;
-        }}
-        // Specify how agenda knob should look like
-        renderKnob={() => {
-          return <View />;
-        }}
-        // Specify what should be rendered instead of ActivityIndicator
-        renderEmptyData={() => {
-          return <View />;
-        }}
-        // Specify your item comparison function for increased performance
-        rowHasChanged={(r1, r2) => {
-          return r1.text !== r2.text;
-        }}
-        // Hide knob button. Default = false
-        hideKnob={false}
-        // When `true` and `hideKnob` prop is `false`, the knob will always be visible and the user will be able to drag the knob up and close the calendar. Default = false
-        showClosingKnob={false}
-        // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-        markedDates={{
-          '2022-03-17': { marked: true },
-        }}
-        // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-        disabledByDefault={false}
-        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly
-        onRefresh={() => console.log('refreshing...')}
-        // Set this true while waiting for new data from a refresh
-        refreshing={false}
-        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView
-        refreshControl={null}
-        // Agenda theme
-        theme={{
-          agendaDayTextColor: 'yellow',
-          agendaDayNumColor: 'green',
-          agendaTodayColor: 'red',
-          agendaKnobColor: 'blue',
-        }}
-        // Agenda container style
-        style={{}}
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={'2017-05-16'}
+        renderItem={renderItem}
       />
     </Container>
   );
