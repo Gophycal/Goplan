43d3f403a624149e58b54727972c151764ad91f9
이승원 <iseung-won@iseung-won-ui-MacBookPro.local>
2022-03-16 04:09:53 +0900

    22/03/16 mk calendar support korean lang / By dummy data, accessing a structure of the Agenda is successed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
-import { Agenda } from 'react-native-calendars';
+import { Agenda, LocaleConfig } from 'react-native-calendars';
 import { TouchableOpacity } from 'react-native';
 import { Card, Paragraph, Avatar } from 'react-native-paper';
 
@@ -10,41 +10,105 @@ const Container = styled.SafeAreaView`
 
 const View = styled.View``;
 
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+    '일요일',
+  ],
+  dayNamesShort: ['월', '화', '수', '목', '금', '토', '일'],
+  today: '오늘',
 };
+LocaleConfig.defaultLocale = 'fr';
+
+// const timeToString = (time) => {
+//   const date = new Date(time);
+//   return date.toISOString().split('T')[0];
+// };
 
 const App = () => {
-  const [items, setItems] = useState({});
+  // const [items, setItems] = useState({});
 
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
+  // const loadItems = (day) => {
+  //   setTimeout(() => {
+  //     for (let i = -15; i < 85; i++) {
+  //       const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+  //       const strTime = timeToString(time);
 
-        if (!items[strTime]) {
-          items[strTime] = [];
+  //       if (!items[strTime]) {
+  //         items[strTime] = [];
 
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-              day: strTime,
-            });
-          }
-        }
-      }
+  //         const numItems = Math.floor(Math.random() * 3 + 1);
+  //         for (let j = 0; j < numItems; j++) {
+  //           items[strTime].push({
+  //             name: 'Item for ' + strTime + ' #' + j,
+  //             height: Math.max(50, Math.floor(Math.random() * 150)),
+  //             day: strTime,
+  //           });
+  //         }
+  //       }
+  //     }
 
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  };
+  //     const newItems = {};
+  //     Object.keys(items).forEach((key) => {
+  //       newItems[key] = items[key];
+  //     });
+  //     setItems(newItems);
+  //   }, 1000);
+  // };
+
+  // const renderItem = (item) => {
+  //   return (
+  //     <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+  //       <Card>
+  //         <Card.Content>
+  //           <View
+  //             style={{
+  //               flexDirection: 'row',
+  //               justifyContent: 'space-between',
+  //               alignItems: 'center',
+  //             }}
+  //           >
+  //             <Paragraph>{item.name}</Paragraph>
+  //             <Avatar.Text label="G" />
+  //           </View>
+  //         </Card.Content>
+  //       </Card>
+  //     </TouchableOpacity>
+  //   );
+  // };
 
   const renderItem = (item) => {
     return (
@@ -59,7 +123,7 @@ const App = () => {
               }}
             >
               <Paragraph>{item.name}</Paragraph>
-              <Avatar.Text label="G" />
+              <Avatar.Text label="승원" />
             </View>
           </Card.Content>
         </Card>
@@ -70,9 +134,18 @@ const App = () => {
   return (
     <Container>
       <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2017-05-16'}
+        // items={items}
+        // loadItemsForMonth={loadItems}
+        items={{
+          '2022-03-22': [{ name: '교생실습 준비' }],
+          '2022-03-23': [{ name: '졸업파티', height: 80 }],
+          '2022-03-24': [],
+          '2022-03-25': [
+            { name: '생일 선물 사러 가기' },
+            { name: '적금 만기일' },
+          ],
+        }}
+        selected={'2022-03-22'}
         renderItem={renderItem}
       />
     </Container>
